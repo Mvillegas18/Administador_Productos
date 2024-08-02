cd8b52f9fdb5c86bba863470f8b815db5eced357
Miguel Angel Villegas <chumilo227@gmail.com>
2024-08-02 17:49:50 -0500

    feat: Update getProductById handler to handle invalid product IDs

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,10 +3,25 @@ import Product from '../models/Product.model';
 
 export const getProducts = async (req: Request, res: Response) => {
 	try {
-		const products = await Product.findAll({
-			order: [['price', 'DESC']],
-		});
+		const products = await Product.findAll();
 		res.json({ data: products });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findByPk(id);
+
+		if (!product) {
+			return res.status(400).json({
+				error: 'Producto no encontrado',
+			});
+		}
+
+		res.json({ data: product });
 	} catch (error) {
 		console.log(error);
 	}
@@ -14,5 +29,18 @@ export const getProducts = async (req: Request, res: Response) => {
 
 export const createProduct = async (req: Request, res: Response) => {
 	const product = await Product.create(req.body);
+	res.json({ data: product });
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const product = await Product.findByPk(id);
+	if (!product) {
+		return res.status(404).json({ error: 'El producto no existe' });
+	}
+
+	await product.update(req.body);
+	await product.save();
 	res.json({ data: product });
 };
