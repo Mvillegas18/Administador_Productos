c754dcd4d04ed2190ade67ee8d02a295cb1371b8
Miguel Angel Villegas <chumilo227@gmail.com>
2024-08-02 15:26:17 -0500

    feat: Add input validation for name and price fields in createProduct handler

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { createProduct } from './handlers/product';
+import { handleInputErrors } from './middleware';
 
 export const router: Router = Router();
 
@@ -8,7 +10,22 @@ router.get('/', (req, res) => {
 	res.json('Desde el metodo get');
 });
 
-router.post('/', createProduct);
+router.post(
+	'/',
+	body('name')
+		.notEmpty()
+		.withMessage('El nombre del producto no puede ir vacio'),
+
+	body('price')
+		.isNumeric()
+		.withMessage('Valor no valido')
+		.notEmpty()
+		.withMessage('El precio del producto no puede ir vacio')
+		.custom((value) => value > 0)
+		.withMessage('Precio no valido'),
+	handleInputErrors,
+	createProduct
+);
 
 router.put('/', (req, res) => {
 	res.json('Desde el metodo put');
