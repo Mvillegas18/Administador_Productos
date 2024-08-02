57dcc70415767a90c5809b31052a0fd95dba3edc
Miguel Angel Villegas <chumilo227@gmail.com>
2024-08-01 20:48:41 -0500

    chore: Update database configuration to include model files

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,4 +3,6 @@ import { Sequelize } from 'sequelize-typescript';
 dotenv.config();
 
 // eslint-disable-next-line no-undef
-export const db = new Sequelize(process.env.DB_CONNECTION);
+export const db = new Sequelize(process.env.DB_CONNECTION, {
+	models: [__dirname + '/../models/**/*.ts'],
+});
